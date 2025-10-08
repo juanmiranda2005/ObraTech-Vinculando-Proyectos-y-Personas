29b62e68a94e1ca756c8638110fc5eed026bf524
juanmiranda2005 <canatemirandajuancito@gmail.com>
2025-10-07 22:32:17 -0500

    Versión inicial del proyecto

---
+++ src/main/java/com/obratech/ObratechApplication.java
@@ -0,0 +1,13 @@
+package com.obratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObratechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObratechApplication.class, args);
+	}
+
+}
